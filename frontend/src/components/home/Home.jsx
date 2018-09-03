53c6a3d3d99ecc7fc05f9ff373ffea92364d059e
amandaacilene <amanda.cilene@hotmail.com>
2018-09-03 01:25:02 -0300

    first commit

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title=" Inicio" subtitle="Projeto Cadastro de Usúario.">
+        <div className='display-4'>Bem Vindo! </div>
+        <hr />
+        <p className="mb-0">Sistema de Cadastro de Usuários em React!</p>
+    </Main>
