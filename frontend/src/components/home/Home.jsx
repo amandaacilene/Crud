550343bead2561a61cba94baed63fd94d13b93f5
Amanda <amanda@M01DEV04.local>
2018-09-03 10:11:53 -0300

    Versão melhora

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -3,7 +3,7 @@ import Main from '../template/Main'
 
 export default props =>
     <Main icon="home" title=" Inicio" subtitle="Projeto Cadastro de Usúario.">
-        <div className='display-4'>Bem Vindo! </div>
+        <div className='display-4'>Bem Vindo </div>
         <hr />
-        <p className="mb-0">Sistema de Cadastro de Usuários em React!</p>
+        <p className="mb-0">Ao Sistema de Cadastro de Usuários produzido com React!</p>
     </Main>
